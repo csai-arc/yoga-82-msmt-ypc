79d8d776823793ec2d0e2a4bf43ee5e703d0e45a
csai-arc <70709024+csai-arc@users.noreply.github.com>
2022-12-27 19:37:36 +0530

    Update __init__.py

--- application/models/classification/__init__.py
+++ application/models/classification/__init__.py
@@ -1,17 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-/*********************************************************************
-*
-*  Copyright (c) 2021, Toshiba Software India Pvt Limited
-*
-*  This file is part of Wafer Defect Classification Sample Training Application Package
-*  File Description : __init__.py
-*  This file describes network libraries to classify defects in Wafer images
-*
-*  All rights reserved.
-*
-*********************************************************************/
-"""
+
 
 from __future__ import absolute_import
 
